c9d31afcf244005df65d6fd8b0f2a4b4a2595e58
lorraineros <43746757+lorraineros@users.noreply.github.com>
2020-03-05 13:39:38 +0000

    momo

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
-// setup canvas
-
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
 function random(min,max) {
   const num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
-
-// define Ball constructor
 
 function Ball(x, y, velX, velY, color, size) {
   this.x = x;
@@ -24,16 +18,12 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-// define ball draw method
-
 Ball.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
 };
-
-// define ball update method
 
 Ball.prototype.update = function() {
   if((this.x + this.size) >= width) {
@@ -56,8 +46,6 @@ Ball.prototype.update = function() {
   this.y += this.velY;
 };
 
-// define ball collision detection
-
 Ball.prototype.collisionDetect = function() {
   for(let j = 0; j < balls.length; j++) {
     if(!(this === balls[j])) {
@@ -72,15 +60,11 @@ Ball.prototype.collisionDetect = function() {
   }
 };
 
-// define array to store balls and populate it
-
 let balls = [];
 
 while(balls.length < 25) {
   const size = random(10,20);
   let ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the adge of the canvas, to avoid drawing errors
     random(0 + size,width - size),
     random(0 + size,height - size),
     random(-7,7),
@@ -90,8 +74,6 @@ while(balls.length < 25) {
   );
   balls.push(ball);
 }
-
-// define loop that keeps drawing the scene constantly
 
 function loop() {
   ctx.fillStyle = 'rgb(216, 194, 239)';
